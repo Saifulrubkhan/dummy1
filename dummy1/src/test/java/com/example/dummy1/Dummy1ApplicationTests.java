ba218bb0bc8b60a21c6f577a04eaa24343d95394
saifulrubkhan <rubsaiful@gmail.com>
2021-01-21 01:57:23 -0500

    Dummy1_project

---
+++ dummy1/src/test/java/com/example/dummy1/Dummy1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dummy1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dummy1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
